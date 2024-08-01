1f51fda8610f063f30e4c1c7191ef05001f9eba6
munaimpro <khanmail2599@gmail.com>
2024-08-01 14:40:38 +0600

    Admin about page front end functionality complete

--- munaimpro.com/app/Http/Controllers/AboutController.php
+++ munaimpro.com/app/Http/Controllers/AboutController.php
@@ -78,7 +78,7 @@ class AboutController extends Controller
             'hero_description' => 'required|string',
             'about_description' => 'required|string',
             'resume_link' => 'string|max:100',
-            'hero_image' => 'mimes:jpeg,jpg,png|max:2048',
+            'hero_image' => 'image|mimes:jpeg,jpg,png|max:2048',
             'about_image' => 'image|mimes:jpeg,jpg,png|max:2048',
         ]);
 
